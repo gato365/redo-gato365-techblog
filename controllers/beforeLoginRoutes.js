42fe342d4add86353ecaf3166f76a0e9ad48caeb
gato365 <immanuelwilliams@gato365.com>
2023-02-08 05:40:19 -0800

    Added routes to before login routes

--- controllers/beforeLoginRoutes.js
+++ controllers/beforeLoginRoutes.js
@@ -1,9 +1,44 @@
 const { Router } = require("express");
 const { User, Post, Comment } = require('../models');
-const apiRouter = require("./api");
+
+
+const beforeLoginRouter = new Router();
+
+
+
+// Display all posts on the home page (before login)
+beforeLoginRouter.get('/', async (req, res) => {
+    let posts = await Post.findAll({
+         include: [
+             {
+                 model: User,
+                 attributes: ['id', 'username'],
+             },
+             {
+                 model: Comment,
+                 include: [
+                     {
+                         model: User,
+                         attributes: ['id', 'username'],
+                     },
+                 ],
+             },
+         ]
+ 
+     });
+ 
+ 
+     posts = posts.map((post) => post.get({ plain: true }));
+ 
+     res.render("home", {
+         posts
+     });
+ 
+ });
 
 
 
 
+module.exports = beforeLoginRouter;
 
-const allRouter = new Router();
+
