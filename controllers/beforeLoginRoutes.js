2eee454cb954f7d2bb743205fd3e63e57a405761
gato365 <immanuelwilliams@gato365.com>
2023-02-08 06:54:27 -0800

    Made beforeLogin look better

--- controllers/beforeLoginRoutes.js
+++ controllers/beforeLoginRoutes.js
@@ -9,29 +9,36 @@ const beforeLoginRouter = new Router();
 // Display all posts on the home page (before login)
 beforeLoginRouter.get('/', async (req, res) => {
     let posts = await Post.findAll({
+
          include: [
              {
                  model: User,
-                 attributes: ['id', 'username'],
+                //  attributes: ['id', 'username'],
              },
              {
                  model: Comment,
                  include: [
                      {
                          model: User,
-                         attributes: ['id', 'username'],
+                        //  attributes: ['id', 'username'],
                      },
                  ],
              },
-         ]
+         ]   
+        
  
      });
  
- 
+    
+
      posts = posts.map((post) => post.get({ plain: true }));
- 
-     res.render("home", {
-         posts
+     
+    //  res.status(200).json(posts);
+
+
+
+     res.render("beforeLoginPage", {
+        posts
      });
  
  });
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const afterLoginRoutes = require('./afterLoginRoutes');
+// const apiRoutes = require('./api');
+// const afterLoginRoutes = require('./afterLoginRoutes');
 const beforeLoginRoutes = require('./beforeLoginRoutes');
 
 router.use('/', beforeLoginRoutes);
-router.use('/dashboard', afterLoginRoutes);
-router.use('/api', apiRoutes);
+// router.use('/dashboard', afterLoginRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
