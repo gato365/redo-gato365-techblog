f85ba416f95b5eb3f36c6683f3f43142d46ff943
gato365 <immanuelwilliams@gato365.com>
2023-02-08 04:35:33 -0800

    Added Posts

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Post extends Model { }
+
+
+Post.init(
+    {
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+
+
+
+
+module.exports = Post;
