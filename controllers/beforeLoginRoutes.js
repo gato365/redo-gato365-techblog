a07e61b67466fa64a94aecbbb6501d924735ccef
gato365 <immanuelwilliams@gato365.com>
2023-02-08 08:22:26 -0800

    Created Loging and signup pages

--- controllers/beforeLoginRoutes.js
+++ controllers/beforeLoginRoutes.js
@@ -10,38 +10,46 @@ const beforeLoginRouter = new Router();
 beforeLoginRouter.get('/', async (req, res) => {
     let posts = await Post.findAll({
 
-         include: [
-             {
-                 model: User,
+        include: [
+            {
+                model: User,
                 //  attributes: ['id', 'username'],
-             },
-             {
-                 model: Comment,
-                 include: [
-                     {
-                         model: User,
+            },
+            {
+                model: Comment,
+                include: [
+                    {
+                        model: User,
                         //  attributes: ['id', 'username'],
-                     },
-                 ],
-             },
-         ]
-        
- 
-     });
- 
-    
+                    },
+                ],
+            },
+        ]
+    });
+    posts = posts.map((post) =>
+        post.get({
+            plain: true
+        }));
 
-     posts = posts.map((post) => post.get({ plain: true }));
-     
     //  res.status(200).json(posts);
 
 
 
-     res.render("beforeLoginPage", {
+    res.render("beforeLoginPage", {
         posts
-     });
- 
- });
+    });
+
+});
+
+// Login page
+beforeLoginRouter.get('/login', async (req, res) => {
+    res.render('login');
+});
+
+// Sign up page
+beforeLoginRouter.get('/signup', async (req, res) => {
+    res.render('signup');
+});
 
 
 
