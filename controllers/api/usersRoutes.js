7d334e85921a8ca323c09bcff4f4f96bbe581e9f
gato365 <immanuelwilliams@gato365.com>
2023-03-02 11:35:51 -0800

    Added comments

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// Create a new user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -12,44 +14,78 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    
+
     res.status(400).json(err);
   }
 });
 
-
+// Login route for existing users
 router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
+  try {
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({
+      where: { email: req.body.email }
+    });
+
+
+
+    // If no user is found, respond with an error message (Wrong email or password)
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again9' });
+      return;
     }
-  });
 
 
-  module.exports = router;
+    // Validate the password
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    // If the password is incorrect, respond with an error message (Wrong email or password)
+    if (!validPassword) {
+      res
+        .status(404)
+        .json({ message: 'Incorrect email or password, please try again8' });
+      return;
+    }
+
+    // If the user is found and the password is correct, log the user in and respond with a success message
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Logout route for existing users
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+
+// For Learning Purposes
+
+// Get All Users
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
