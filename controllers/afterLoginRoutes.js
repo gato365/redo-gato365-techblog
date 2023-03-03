c701c76a39c75d76e1f96a024c907b170c527ba5
gato365 <immanuelwilliams@gato365.com>
2023-03-02 16:46:13 -0800

    Made Login authorization

---
+++ controllers/afterLoginRoutes.js
@@ -0,0 +1,166 @@
+const afterLoginRouter = require('express').Router();
+const sequelize = require('../config/connection');
+const {
+    User,
+    Post,
+    Comment
+} = require('../models');
+
+// WithAuth middleware to prevent access to routes
+const withAuth = require('../utils/auth');
+
+// Display all posts on the home page (after login)
+
+afterLoginRouter.get('/', async (req, res) => {
+
+    try {
+        let posts = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    //  attributes: ['id', 'username'],
+                },
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            //  attributes: ['id', 'username'], 
+                        },
+                    ],
+                },
+            ]
+        });
+
+
+        // If there is an error, display the error
+        if (!posts) {
+            res.status(404).json({
+                message: 'No posts found'
+            });
+        }
+
+
+        // Serialize data so the template can read it
+        posts = posts.map((post) =>
+            post.get({
+                plain: true
+            })
+        );
+
+
+        // Display all posts on the home page (after login)
+        res.status(200).render("afterLoginPage", {
+            posts
+        });
+
+
+
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+
+// Display a single post on the post page (after login)
+afterLoginRouter.get('/post/:id', async (req, res) => {
+    try {
+        // Find the post by its id
+        let post = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    //  attributes: ['id', 'username'],
+                },
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            //  attributes: ['id', 'username'],
+                        },
+                    ],
+                },
+            ]
+        });
+        // Check if the post exists
+        if (!post) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        // Serialize data so the template can read it
+        post = post.get({
+            plain: true
+        });
+
+        // Display a single post on the post page (after login)
+        res.status(200).render("post", {
+            post
+        });
+
+    } catch (err) {
+        // If there is an error, display it
+        res.status(500).json(err);
+    }
+
+});
+
+
+// Edit a post
+afterLoginRouter.get('/edit/:id', async (req, res) => {
+    try {
+        // Find the post by its id
+        let post = await Post.findOne({
+            where: {    
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: User,
+                    //  attributes: ['id', 'username'],
+                },
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            //  attributes: ['id', 'username'],
+                        },
+                    ],
+                },
+            ]
+        });
+        // Check if the post exists
+        if (!post) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        // Serialize data so the template can read it
+        post = post.get({
+            plain: true
+        });
+
+        // Display a single post on the post page (after login)
+        res.status(200).render("editPost", {
+            post
+        });
+
+    } catch (err) {
+        // If there is an error, display it
+        res.status(500).json(err);
+    }
+
+});
+
+
+
+
+
+// Export the router
+module.exports = afterLoginRouter;
